2b6ca8231cabd55761394e966b0241026ba8e8fc
Mateus Zitelli <zitellimateus@gmail.com>
2014-01-24 19:06:45 -0200

    Optimizing the UX of the train maker page

--- traintool/static/js/maker.js
+++ traintool/static/js/maker.js
@@ -5,12 +5,18 @@ $(document).ready(function(){
 	var $exerciciosDiv = $("#exerciciosDiv");
 	var $openExerciciosMenus = $(".list-group-item.clickable");
 	var $open_panel = $(".open-panel");
+	var $edit_day = $("#edit_day");
+	var $day_texts = $(".day_texts");
+	var $textareas = $("textarea");
+	var $inputs = $("input");
+	var $new_day_btn = $("#new_day_btn");
 
 	var daysContracted = false;
 	var listOptions = {
 		valueNames: ['name']
 	};
 	var exList = new List('exerciciosDiv', listOptions);
+
 	$exerciciosDiv.hide();
 
 	//Functions
@@ -37,20 +43,36 @@ $(document).ready(function(){
 		daysContracted = !daysContracted;
 		toggleContractionDiv($daysDiv, 12, 8);
 		toggleContractionDiv($exerciciosDiv, 1, 4, toggleExerciciosDiv);
+		saveState();
 	}
 
-	doOnClick($addExercicio, toggleExerciciosList);
+	var save_element_data = function(){
+		var elementId = $(this).attr("id");
+		var elementValue = $(this).val();
+		localStorage[elementId] = elementValue;
+	}
+
+	var saveState = function(){
+		$textareas.each(save_element_data);
+		$inputs.each(save_element_data);
+	}
+
+	var addNewDay = function(){
+		
+	}
 
 	$openExerciciosMenus.each(function(){
 		var openNext = function(){
 			$(this).next().find("div").fadeToggle();
 		}
-
 		$this = $(this);
 		doOnClick($this, openNext);
 	});
 
 	$open_panel.each(function(){
 		$(this).find("div").hide();
-	})
+	});
+
+	doOnClick($addExercicio, toggleExerciciosList);
+	doOnClick($new_day_btn, )
 });
--- traintool/trainMaker/views.py
+++ traintool/trainMaker/views.py
@@ -1,4 +1,47 @@
+#-*- coding:utf-8 -*-
 from django.shortcuts import render
+import uuid
+
+def uid():
+	return uuid.uuid1().hex
+
+class Exercicio:
+	def __init__(self, nome, descricao, series, ordem = 0):
+		self.nome = nome
+		self.descricao = descricao
+		self.ordem = ordem
+		self.objectType = "exercicio"
+		if type(series) == str:
+			self.series = " X ".join(series.split(','))
+		elif type(series) == list:
+			self.series = " X ".join(series)
+
+class Relacao:
+	def __init__(self, relactionType, time, description):
+		self.relactionType = relactionType
+		self.time = time
+		self.description = description
+		self.objectType = "relacao"
+
+class Dia:
+	def __init__(self, nome, detalhes, objects = []):
+		self.nome = nome
+		self.uid = uid()
+		self.detalhes = detalhes
+		self.objects = objects
+
+class Treino:
+	def __init__(self, nome, detalhes, dias = []):
+		self.nome = nome
+		self.detalhes = detalhes
+		self.dias = dias
+		self.uid = uid()
 
 def index(request):
-	return render(request, 'trainMaker/index.html', {})
+	exercicios_dia_0 = [Exercicio("Supino Reto", "Aqui ficará a descrição do exercício,\
+		execução e dicas para melhor rendimento, imagens e gifs seriam bem-vindos.", "10,10,10", ""),
+	Relacao('descanso', 30, 'Vai beber água.',)]
+	dias = [Dia("Peito/Tríceps", "Treino pesadão pra ficar maromba!!1!", exercicios_dia_0)]
+	treino = Treino("Teste", "Teste de treino pra ficar marooombaaa", dias)
+	context = {"treino":treino}
+	return render(request, 'trainMaker/index.html', context)
